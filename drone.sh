438dd27a61293bde11e3fdfd90bd75509438614a
Johan Planchon <dev@planchon.xyz>
2020-05-15 07:58:30 +0200

    patch(CICD): Bad source url

--- drone.sh
+++ drone.sh
@@ -22,7 +22,7 @@ export GH_CHK_IN_PRIVATE_KEY_FILE=/temp.private-key.pem
 
 name=$DRONE_STAGE_NAME
 head_sha=$DRONE_COMMIT_SHA
-details_url=$DRONE_SYSTEM_PROTO://$DRONE_SYSTEM_HOSTNAME/$DRONE_REPO/$DRONE_STAGE_NUMBER/$DRONE_STEP_NUMBER
+details_url=https://drone.planchon.xyz/$DRONE_REPO/$DRONE_BUILD_NUMBER/$DRONE_STAGE_NUMBER
 stage_status="completed" # Can be 'queued', 'in_progress' or 'completed'
 repo=$DRONE_REPO
 started_at=$(date -d @$DRONE_BUILD_STARTED +'%Y-%m-%dT%H:%M:%SZ')
